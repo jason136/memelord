71154c7f21f6650e584a9759b91e0979ec6ac965
Jason Wiemels <jason.wiemels@gmail.com>
2022-02-18 23:59:52 -0800

    added multithreading

--- main - Copy.py
+++ main - Copy.py
@@ -1,6 +1,4 @@
-import praw, os, requests
-import unicodedata, re
-import threading, queue
+import asyncio, praw, os, requests, unicodedata, re
 import tokens
 
 def legalize(text, allow_unicode=False):
@@ -12,9 +10,9 @@ def legalize(text, allow_unicode=False):
     text = re.sub(r'[^\w\s-]', '', text.lower())
     return re.sub(r'[-\s]+', '-', text).strip('-_')
 
-def download(url, filepath):
+async def download(url, filepath):
     try:
-        print(f'downloading {url} in {images_path}')
+        print(f'downloading {url} in {filepath}')
         r = requests.get(url, allow_redirects=True)
         with open(filepath, 'wb') as file:
             file.write(r.content)
@@ -24,39 +22,27 @@ def download(url, filepath):
         if download(url, filepath):
             return True
 
-q = queue.Queue()
-number_of_threads = 30
-
-def worker():
-    while True:
-        meme = q.get()
-        download(meme[0], meme[1])
-        q.task_done()
-
-reddit = praw.Reddit(client_id=tokens.REDDIT_ID, client_secret=tokens.REDDIT_SECRET, user_agent=tokens.REDDIT_AGENT)
+reddit = praw.Reddit(client_id=tokens.REDDIT_ID, client_secret=tokens.REDDIT_SECRET, user_agent=tokens.REDDIT_AGENT, check_for_async=False)
 subreddit = reddit.subreddit('memes')
 
 images_path = './images/'
 image_exts = ['.png', '.jpg', '.jpeg']
 images = {}
 
-def main():
+async def main():
     posts = subreddit.top('all', limit=None)
     if not os.path.exists(images_path):
         os.makedirs(images_path)
 
-    for i in range(number_of_threads):
-        t = threading.Thread(target=worker)
-        t.daemon = True
-        t.start()
-
+    tasks = []
     for post in posts:
         title = post.title
         url = post.url
         _, ext = os.path.splitext(url)
         filepath = images_path + legalize(title) + ext
         if ext in image_exts:
-            q.put([url, filepath])
-    q.join()
+            print('add to tasks')
+            tasks.append(download(url, filepath))
+    await asyncio.gather(*tasks)
 
-main()
+asyncio.run(main())
