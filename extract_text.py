0d183be1d74bea1ac8eebdc17786398ce42a8c36
Jason <jason.wiemels@gmail.com>
2022-03-26 21:16:43 -0700

    ml begun

--- extract_text.py
+++ extract_text.py
@@ -9,32 +9,30 @@ import globals
 number_of_threads = 10
 q = queue.Queue()
 w = queue.Queue()
-extracted_count = 0
 
+extracted_text = []
 def worker():
     while True:
         meme = q.get()
         img = Image.open(meme[0]['filenames'][meme[1]])
-        #text = pytesseract.image_to_string(img, lang='eng')
         data = pytesseract.image_to_data(img, output_type='data.frame')
         data = data[data.conf != -1]
         words = data['text'].tolist()
         confs = data['conf'].tolist()
 
-        line_conf = []
         text = ''
-    
         for x in range(len(words)):
             word = unicodedata.normalize('NFKD', str(words[x])).encode('ascii', 'ignore').decode('ascii')
-            if word.replace('\n', ' ').strip():
-                line_conf.append((word, round(confs[x], 3)))
-                if round(confs[x], 3) > 50:
-                    text += word.strip() + ' '
+            if not word.replace('\n', ' ').strip(): continue
+            if round(confs[x], 3) > 50:
+                text += word.strip() + ' '
+        if text.strip(): extracted_text.append(text)
 
         print(f'text extracted: {text}')
         w.put([meme[0], meme[1], text])
         q.task_done()
 
+extracted_count = 0
 def save():
     while True:
         meme = w.get()
@@ -75,7 +73,22 @@ def main():
         dataframe = pd.DataFrame.from_dict(data)
         csv = dataframe.to_csv(f'{memes_root}/{date}/{date}.csv', index=True, header=True)
 
-    input(f"{extracted_count} lines extracted and {len(dicts.keys())} csv's written")
+    print(f"{extracted_count} lines extracted and {len(dicts.keys())} csv's written")
+    
+    filename = f'{memes_root}/extracted_text.txt'
+    lines = []
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            lines = [line.replace('\n', '').strip() for line in f]
+        print(lines)
+
+    with open(filename, 'w') as f:
+        for text in lines:
+            f.write(f'{text}\n')
+        for text in extracted_text:
+            if text.replace('\n', '').strip() not in lines:
+                f.write(f'{text}\n')
+    input(f'{filename} written')
 
 if __name__ == '__main__':
     main()
